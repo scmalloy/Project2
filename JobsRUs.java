88855f3ed7ffca35ead88aada1ba62b43fe282a2
Anthony Salazar <127241421+anthonymsalazar@users.noreply.github.com>
2023-05-18 08:40:39 -0500

    Update JobsRUs.java
    Missing bracket and formating

--- JobsRUs.java
+++ JobsRUs.java
@@ -129,37 +129,37 @@ public class JobsRUs extends Jobs {
 	}
 	
 	public static void jobSelection() {
-	// To call to the 
-	// Field field = new Field();
-	// field.field();
-	Scanner in = new Scanner(System.in); 
-	String selectionType;
+		// To call to the 
+		// Field field = new Field();
+		// field.field();
+		Scanner in = new Scanner(System.in); 
+		String selectionType;
 	
-	do {
-		System.out.println("\n*********Job Selection*********");
-		System.out.println("Please choose how you would like to filter your job search: "
-				+ "\nA.) All jobs"
-				+ "\nB.) By pay range"
-				+ "\nC.) By location"
-				+ "\nD.) By job field");
-			selectionType = in.nextLine();
+		do {
+			System.out.println("\n*********Job Selection*********");
+			System.out.println("Please choose how you would like to filter your job search: "
+					+ "\nA.) All jobs"
+					+ "\nB.) By pay range"
+					+ "\nC.) By location"
+					+ "\nD.) By job field");
+				selectionType = in.nextLine();
 		
-		System.out.println("\n*********************************{ processing........ }*********************************\n");
-	} while (!selectionType.equalsIgnoreCase("A") && !selectionType.equalsIgnoreCase("B") 
-			&& !selectionType.equalsIgnoreCase("C") && !selectionType.equalsIgnoreCase("D"));
+			System.out.println("\n*********************************{ processing........ }*********************************\n");
+		} while (!selectionType.equalsIgnoreCase("A") && !selectionType.equalsIgnoreCase("B") 
+				&& !selectionType.equalsIgnoreCase("C") && !selectionType.equalsIgnoreCase("D"));
 	
-	if (selectionType.equalsIgnoreCase("A")) {
-		Jobs job = new Jobs();
-		job.allJobs();
-	} else  if (selectionType.equalsIgnoreCase("B")) {
-		PayRange pay = new PayRange();
-		pay.payRange();
-	} else  if (selectionType.equalsIgnoreCase("C")) {
-		Location loc = new Location();
-		loc.location();
-	} else {
-		Field field = new Field();
-		field.field();
-	}
-	
+		if (selectionType.equalsIgnoreCase("A")) {
+			Jobs job = new Jobs();
+			job.allJobs();
+		} else  if (selectionType.equalsIgnoreCase("B")) {
+			PayRange pay = new PayRange();
+			pay.payRange();
+		} else  if (selectionType.equalsIgnoreCase("C")) {
+			Location loc = new Location();
+			loc.location();
+		} else {
+			Field field = new Field();
+			field.field();
+		}
+	}	
 }
