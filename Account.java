979ac71d889d9882a975259a634c6c2cdf0d3b31
Anthony Salazar <127241421+anthonymsalazar@users.noreply.github.com>
2023-05-20 17:33:03 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,14 +1,14 @@
-package com.skillstorm;
+package com.skillstorm.project;
 
 import java.util.ArrayList;
 
 public class Account {
 	private String name;
-	private String userID;
+	private String username;
 	private String password;
 	private String email;
 	private String phone;
-	private static ArrayList<String> applications;
+	private static ArrayList<String> applications = new ArrayList<>();
 	
 	public Account() {}
 
@@ -23,18 +23,17 @@ public class Account {
 	public static void addToApp(String i) {
 		applications.add(i);
 	}
-	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getUserID() {
-		return userID;
+	public String getUsername() {
+		return username;
 	}
-	public void setUserID(String userID) {
-		this.userID = userID;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getPassword() {
 		return password;
@@ -57,7 +56,14 @@ public class Account {
 	public ArrayList<String> getApplications() {
 		return applications;
 	}
-	public void setApplications(ArrayList<String> application) {
-		this.applications = application;
+	public void setApplications(ArrayList<String> applications) {
+		this.applications = applications;
 	}
+	public String toString() {
+	    String results = "+";
+	    for(String a : applications) {
+	        results += a.toString();
+	    }
+	    return results;
+	}	
 }
